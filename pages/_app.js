7ba442e44a24a21852ae6c915b0c14c033fa5fe6
Bruno Guilhem <boguilhem@gmail.com>
2022-12-06 19:31:14 -0300

    feat: adding Footer to global pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Image from 'next/image';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{' '}
+        <span className="logo">
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </span>
+      </a>
+    </footer>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
