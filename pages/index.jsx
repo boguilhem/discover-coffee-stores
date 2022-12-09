57b446f1cad66ff5c1e4e70b9b47f23f9428c577
Bruno Guilhem <boguilhem@gmail.com>
2022-12-09 19:27:44 -0300

    feat: Adding banner

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,12 @@
 import Head from 'next/head';
+import Banner from '../components/banner';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const handleOnBannerBtnClick = () => {
+    console.log('Banner Button Clicked');
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,7 +16,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Coffee Connoisseur</h1>
+        <Banner
+          buttonText={'View stores nearby'}
+          handleOnClick={handleOnBannerBtnClick}
+        />
       </main>
     </div>
   );
